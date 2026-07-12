0d582d2cdab216f03b164f3c5683947bfaa0e8e9
EnlistedSpring8 <onnzki@gmail.com>
2026-07-12 22:21:12 +0300

    J: Made LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,18 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+    
+    class LeafNode(HTMLNode): # type: ignore
+
+        def __init__(self, tag: str, value: str, props: dict[str, str] = None):
+            super().__init__(tag=tag, value=value, props=props)
+        
+        def to_html(self) -> str:
+            if self.value is None:
+                raise ValueError("LeafNode must have a value.")
+            if self.tag is None:
+                return self.value
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+        def __repr__(self):
+            return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -15,5 +15,23 @@ class TestHTMLNode(unittest.TestCase):
         expected_repr = "HTMLNode(tag=div, value=Hello, World!, children=[], props={'class': 'my-class', 'id': 'my-id'})"
         self.assertEqual(repr(node), expected_repr)
     
+    def test_leaf_to_html_p(self):
+        leaf = HTMLNode.LeafNode(tag="p", value="Hello, World!", props={"class": "my-class"})
+        self.assertEqual(leaf.to_html(), '<p class="my-class">Hello, World!</p>')
+
+    def test_leaf_to_html_no_tag(self):
+        leaf = HTMLNode.LeafNode(tag=None, value="Hello, World!")
+        self.assertEqual(leaf.to_html(), 'Hello, World!')
+    
+    def test_leaf_to_html_no_value(self):
+        leaf = HTMLNode.LeafNode(tag="p", value=None)
+        with self.assertRaises(ValueError):
+            leaf.to_html()
+    
+    def test_leaf_repr(self):
+        leaf = HTMLNode.LeafNode(tag="p", value="Hello, World!", props={"class": "my-class"})
+        expected_repr = "LeafNode(tag=p, value=Hello, World!, props={'class': 'my-class'})"
+        self.assertEqual(repr(leaf), expected_repr)
+        
 if __name__ == "__main__":
     unittest.main()
